e8a2742e972fe6dc90c9521ecc013d2481b5a474
DocWert848 <ruskhay1703@yandex.ru>
2025-11-19 14:00:02 +0300

    Update задание 5.py

--- задание 5.py
+++ задание 5.py
@@ -25,24 +25,23 @@ print (mass)
 bolshe_0 = []
 nol_0 = []
 menshe_0 = []
+summ_mal = 0
+summ_bol = 0
 spusok = int(input("Введите количество чисел: "))
 for i in range(spusok):
     i += 1
     num = int(input(f"Введите {i} число: "))
     if num < 0:
         menshe_0.append(num)
+        summ_mal += num 
     elif num == 0:
         nol_0.append(num)   
     elif num > 0:
         bolshe_0.append(num)
-summ_mal = 0
-while True:
-    if num < 0 in menshe_0:
-        summ_mal += num
-    else:
-        print (summ_mal)
-        break
-print (bolshe_0)
+        summ_bol += num
+        arf = summ_bol/ len(bolshe_0) 
+print (f'положительные значения: {arf} {bolshe_0}')
+print (f'отрицательные значения: {summ_mal} {menshe_0}')
 o = 0
 while True:
     if 0 in nol_0:
